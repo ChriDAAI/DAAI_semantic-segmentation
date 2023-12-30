57d03529c0a37e1b93f93b9e502c1b680ab41215
ChriDAAI <155232054+ChriDAAI@users.noreply.github.com>
2023-12-30 12:03:02 +0100

    Create cityscape.py

---
+++ cityscape.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+import os
+import numpy as np
+from torch.utils.data import Dataset
+from PIL import Image
+import torch
+import torchvision.transforms as T
+
+
+class CityScapes(Dataset):
+    def __init__(self, mode, args):
+      super(CityScapes, self).__init__()
+      self.path = "/content/Cityscapes/Cityspaces"    #To find the dataset
+      self.mode = mode                                #Train or val
+      self.data, self.label = self.data_loader()      #To extract both label and image
+      self.transformed_data = T.Compose([
+          T.ToTensor(),
+          T.Normalize([0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+          ])
+
+      self.width=args.crop_width
+      self.height = args.crop_height
+
+    def __getitem__(self, idx):
+
+     image = self.pil_loader(self.data[idx],  'RGB')
+     label = self.pil_loader(self.label[idx],  'L')
+
+     tensor_image = self.transformed_data(image)
+     tensor_label =torch.from_numpy(np.array(label))
+
+    def __len__(self):
+       return len(self.data)
+
+    def pil_loader(self, p, mode):
+      with open(self.path+p, 'rb') as f:
+            img = Image.open(f)
+            return img.convert(mode).resize((self.width, self.height), Image.NEAREST)
+
+    def data_loader(self):
+      data, label = []
+      categories = ["images/", "gtFine/"]
+      for c in categories:
+       if os.path.exists(self.path+t):
+                # get files from directory
+                for root, dirs, files in os.walk(self.path+t):
+                    if self.mode in root:
+                        for file in files:
+                            file_path = os.path.join(root, file)
+                            # get path in mode
+                            relative_path = os.path.relpath(file_path, self.path)
+                            if t=="images/":
+                                data.append(relative_path)
+                            else:
+                                if file_path.split("gtFine_")[1] == "labelTrainIds.png":
+                                    label.append(relative_path)
+      return sorted(data), sorted(label)
