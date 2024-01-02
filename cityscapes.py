3fc1e59c3a329caa93e4e20e7c1510b74de47055
ChriDAAI <155232054+ChriDAAI@users.noreply.github.com>
2024-01-02 11:45:33 +0100

    Update cityscapes.py

--- cityscapes.py
+++ cityscapes.py
@@ -15,7 +15,7 @@ import json
 from transformation import *
 
 class CityScapes(Dataset):
-    def __init__(self, path, mode='train', cropsize=(640, 480), 
+    def __init__(self, path, mode='train', cropsize=(1024, 512), 
     randomscale=(0.125, 0.25, 0.375, 0.5, 0.675, 0.75, 0.875, 1.0, 1.25, 1.5), *args, **kwargs):
         super(CityScapes, self).__init__(*args, **kwargs)
         self.path =  "/content/Cityscapes/Cityspaces"    #To find the dataset
@@ -30,13 +30,13 @@ class CityScapes(Dataset):
           ])
         
         self.trans_train = Compose([
-           ColorJitter(
+           #ColorJitter(
                brightness = 0.5,
                contrast = 0.5,
                saturation = 0.5),
            HorizontalFlip(),
            # RandomScale((0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0)),
-           RandomScale(randomscale),
+           #RandomScale(randomscale),
            # RandomScale((0.125, 1)),
            # RandomScale((0.125, 0.25, 0.375, 0.5, 0.675, 0.75, 0.875, 1.0)),
            # RandomScale((0.125, 0.25, 0.375, 0.5, 0.675, 0.75, 0.875, 1.0, 1.125, 1.25, 1.375, 1.5)),
