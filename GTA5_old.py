99a88c17016828b575f328660017aa2ea2fe0c7c
ChriDAAI <155232054+ChriDAAI@users.noreply.github.com>
2024-01-16 21:16:53 +0100

    Update GTA5_old.py

--- GTA5_old.py
+++ GTA5_old.py
@@ -21,6 +21,7 @@ class GTA5Dataset(Dataset):
         self.labels_dir_trainID = os.path.join(self.path, 'TrainID/')                                            #To load the path of the labels (/content/GTA5/TrainID)
         self.image_files = sorted(os.listdir(self.images_dir))                                                   #To load the path containg the names of the images ('00001.png')
         self.label_colored_files = sorted(os.listdir(self.labels_dir_colored))                                   #To load the path containg the names of the labels ('00001.png')
+        self.img = os.path.join('images/', self.image_files)
         self.width = 1024                                                                                        
         self.height = 512
         self.transform_data = transforms.Compose([ 
@@ -29,16 +30,18 @@ class GTA5Dataset(Dataset):
         ])
         self.label = sorted(from_label_to_TrainID(self.label_colored_files,self.label_info, self.labels_dir_colored, self.height, self.width))           #Convert label to TrainID format
 
+    def pil_loader(self, p, mode):
+        with open(self.path+p, 'rb') as f:
+            img = Image.open(f)
+            return img.convert(mode).resize((self.width, self.height), Image.NEAREST)
+
+
     def __len__(self):
         return len(self.image_files)                                            
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.images_dir, self.image_files[idx])                                          #Join (/content/GTA5/images) and '00001.png
-        label_name = os.path.join(self.path, self.label[idx])                                                    #This is because self.label has already the path TrainID/
-        with open(img_name, 'rb') as f: 
-            image = Image.open(f).convert('RGB').resize((self.width, self.height), Image.NEAREST)                #I open the image, resize and convert in RGB
-        with open(label_name, 'rb') as b:
-            label = Image.open(label_name).convert('L').resize((self.width, self.height), Image.NEAREST)        #I open the TrainID, resize and convert in L
+        image = self.pil_loader(self.img[idx], 'RGB')
+        label = self.pil_loader(self.label[idx], 'L')
         
         tensor_image = self.transform_data(image)                                                               #To have a tensor
         tensor_label = torch.from_numpy(np.array(label))                                                        #To have a tensor
