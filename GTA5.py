66b295db25e2460632f7dd1220539b47bca5002d
ChriDAAI <155232054+ChriDAAI@users.noreply.github.com>
2024-01-16 17:10:08 +0100

    Update GTA5.py

--- GTA5.py
+++ GTA5.py
@@ -1,64 +1,71 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
-import os
-import pandas as pd
-import numpy as np
-from PIL import Image
 from torch.utils.data import Dataset
-from utils import get_label_info_custom
-from utils import from_label_to_TrainID
 from torchvision import transforms
 import torch
+import os
+from collections import namedtuple
+from pprint import pprint
+from PIL import Image
+import numpy as np
+import pandas as pd
+from utils import get_label_info_custom
+from utils import from_label_to_TrainID
+
 
 class GTA5Dataset(Dataset):
     def __init__(self, mode):
-        super(GTA5Dataset, self).__init__()
-        self.path = "/content/GTA5"                                                                              #Main GTA5 directory 
-        self.mode = mode                                                                                         #Train or validation
+        super(GTA5, self).__init__()
+        self.path = "/content/GTA5/"
+        self.mode = mode
         self.label_info = get_label_info_custom('/content/DAAI_semantic-segmentation/GTA5.csv')                  #I create the list with the info coming from the .csv
         self.images_dir = os.path.join(self.path, 'images/')                                                     #To load the path of the images (/content/GTA5/images)
         self.labels_dir_colored = os.path.join(self.path, 'labels/')                                             #To load the path of the labels (/content/GTA5/labels)
         self.labels_dir_trainID = os.path.join(self.path, 'TrainID/')                                            #To load the path of the labels (/content/GTA5/TrainID)
-        self.image_files, self.label_colored_files = self.loader()                                               #To have '0000x.png' as sorted list both for images and for labels
-        self.width = 1024                                                                                        
-        self.height = 512
+        self.data, self.label_colored = self.data_loader()
         self.transform_data = transforms.Compose([ 
-            transforms.ToTensor(),                 
+            transforms.ToTensor(),                 # Converte l'immagine in un tensore
             transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
         ])
-        self.label = sorted(from_label_to_TrainID(self.label_colored_files,self.label_info, self.labels_dir_colored, self.height, self.width))           #Convert label to TrainID format
+        self.width = 1024
+        self.height = 512
+        self.label=from_label_to_TrainID(self.label_colored_files,self.label_info, self.labels_dir_colored, self.height, self.width)
+        #self.data_augmentation= DataAugmentation()
+        #self.enable_da = enable_da
 
-                                                
+    def pil_loader(self, p, mode):
+        with open(self.path+p, 'rb') as f:
+            img = Image.open(f)
+            return img.convert(mode).resize((self.width, self.height), Image.NEAREST)
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.images_dir, self.image_files[idx])                                          #Join (/content/GTA5/images/) and '00001.png'
-        label_name = os.path.join(self.path, self.label[idx])                                                    #This is because self.label has already the path TrainID/
-        with open(img_name, 'rb') as f: 
-            image = Image.open(f).convert('RGB').resize((self.width, self.height), Image.NEAREST)                #I open the image, resize and convert in RGB
-        with open(label_name, 'rb') as b:
-            label = Image.open(label_name).convert('L').resize((self.width, self.height), Image.NEAREST)        #I open the TrainID, resize and convert in L
-        
-        tensor_image = self.transform_data(image)                                                               #To have a tensor
-        tensor_label = torch.from_numpy(np.array(label))                                                        #To have a tensor
-        return tensor_image, tensor_label
-      
-    def __len__(self):
-        return len(self.image_files)
+        image = self.pil_loader(self.data[idx], 'RGB')
+        label = self.pil_loader(self.label[idx], 'L')
+        #if self.enable_da and np.random.rand()<=0.5:
+         #   image, label= self.data_augmentation(image, label)
+        tensor_image = self.transform_data(image)
+        tensor_label = torch.from_numpy(np.array(label))  
+        return tensor_image, tensor_label 
 
-    def loader(self):
-      img = []
-      lbs = []
-      domain = ["labels/", "images/"]
-      for d in domain:
-        for root, dirs, files in os.walk(self.path+d):
-          for file in files:
-            file_path = os.path.join(root, file)
-            relative_path = os.path.relpath(file_path, self.path)
-            if d=="images/":
-              img.append(os.path.basename(relative_path))          #00001.png
-            elif d=="labels/":
-              lbs.append(os.path.basename(relative_path))
-            if len(img) == len(lbs):
-              break
-      return sorted(img), sorted(lbs)      
+    def __len__(self):
+        return len(self.data)
+    
+    def data_loader(self):
+        img= []
+        lbl = []
+        domain = [ "labels/","images/"]
+        
+        for d in domain:
+            for root, dirs, files in os.walk(self.path+d):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    relative_path = os.path.relpath(file_path, self.path)
+                    if d=="images/":
+                        img.append(relative_path)
+                    else:
+                        lbl.append(relative_path)
+                    if len(data)==len(lbl):
+                        break
+        return sorted(img), sorted(lbl)
+           
         
